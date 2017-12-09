f8d499620a27935298d61a8da839af10264c306f
JamesRevere <34389449+JamesRevere@users.noreply.github.com>
2017-12-08 22:30:12 -0700

    Added the meat and taters
    Modified the README to include more info, and added the script itself.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# how much are you worth
+# How Much Are You Worth?
+Have you ever wondered how much you would sell for if you were split up into your individual elements? 
+Well, now you can with this new handy tool avalable for free under the MIT licence!
+## How does it work?
+Simply enter your weight in pounds (yes, we Americans are backward like that),
+and it will tell you your apoximate* value in USD.
+/* Program does not account for inflation or the shifting prices of elements.
---
+++ how_much_are_you_worth.rb
@@ -0,0 +1,34 @@
+# How Much Are You Worth?
+# Have you ever wondered how much you would sell for if you were split up into your individual elements? Well, now you can!
+
+print "How much do you weigh? "
+weightLBS = gets.chomp.to_i
+
+elements = Hash.new
+price = {o: 0.30, c: 2.40, h: 12.00, n: 0.40, ca: 11.00, p: 4.00, k: 85.00, s: 0.25, na: 7.00, cl: 0.15}
+total = []
+
+weightKGS = weightLBS * 0.4536
+weightGMS = weightKGS * 1000
+
+elements[:o] = weightGMS * 0.65
+elements[:c] = weightGMS * 0.18
+elements[:h] = weightGMS * 0.10
+elements[:n] = weightGMS * 0.03
+elements[:ca] = weightGMS * 0.015
+elements[:p] = weightGMS * 0.01
+elements[:k] = weightGMS * 0.0035
+elements[:s] = weightGMS * 0.0025
+elements[:cl] = elements[:na] = weightGMS * 0.0015
+
+elements.each do |element, value|
+	value /= 100
+	total.push(value * price[element])
+end
+
+cost = 0
+total.each do |value|
+	cost += value
+end
+
+puts cost
