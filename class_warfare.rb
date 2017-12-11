2209bc0cc196057e92a7d9acd4a4dcf18b20b0cc
gbourdon <34389449+JamesRevere@users.noreply.github.com>
2017-12-10 19:41:15 -0700

    Added a "quantity" value
    Added a method of storeing the quantity of an element.

--- class_warfare.rb
+++ class_warfare.rb
@@ -2,6 +2,8 @@ class Element
     attr_reader :price
     attr_reader :ammount
 
+    attr_accessor :quantity
+
 	def initialize(price, ammount)
 		@price = price
 		@ammount = ammount
@@ -20,5 +22,7 @@ end
 
 o = Element.new(0.30, 0.65)
 p o
+o.quantity = 15
+puts o.quantity
 puts o.price
 puts o.ammount
