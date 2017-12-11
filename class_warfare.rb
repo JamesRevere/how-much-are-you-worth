9fda8462b24f6797eb69a0ff96157b85110eaec7
gbourdon <34389449+JamesRevere@users.noreply.github.com>
2017-12-10 19:33:39 -0700

    Updated variable access
    Switched acessing stuff like price and amount from def to attr_reader.

--- class_warfare.rb
+++ class_warfare.rb
@@ -1,17 +1,24 @@
 class Element
+    attr_reader :price
+    attr_reader :ammount
+
 	def initialize(price, ammount)
 		@price = price
 		@ammount = ammount
 	end
-
+=begin
 	def price
 		@price
 	end
 
 	def ammount
 		@ammount
-	end
+    end
+=end
 end
+
 
 o = Element.new(0.30, 0.65)
 p o
+puts o.price
+puts o.ammount
