66011c22730ee5acb6ac7c9da59f52bd99319fdf
gbourdon <34389449+JamesRevere@users.noreply.github.com>
2017-12-10 20:05:00 -0700

    Wumpus Hunting
    Removed the "Quantity" variable, since it  would break the program's structure.

--- class_warfare.rb
+++ class_warfare.rb
@@ -12,15 +12,13 @@ class Element
     attr_reader :price # The price of the element
     attr_reader :ammount # What percentage of the element is in your body.
 
-    attr_accessor :quantity
-
 	def initialize(price, ammount) # What hapens when the class is created
 		@price = price
 		@ammount = ammount
     end
     
     def quantity_init(grams) # Initalizes how much is in your body
-        @quantity = grams * @ammount
+        grams * @ammount
     end
 
 =begin
@@ -37,8 +35,7 @@ end
 
 o = Element.new(0.30, 0.65)
 p o
-o.quantity = 15
-puts o.quantity
+
 puts o.price
 puts o.ammount
 puts [25, 25, 50].total
