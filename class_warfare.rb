ae55fcdd77cd8faa4da34304e41a810d831a6706
gbourdon <34389449+JamesRevere@users.noreply.github.com>
2017-12-10 20:00:16 -0700

    Docs, generation, and sweet Array mods.
    Added documentation for the elements, an easy way to get how much of an element is in you based on it's value in grams, and a way to get the total of an array's numbers.

--- class_warfare.rb
+++ class_warfare.rb
@@ -1,13 +1,28 @@
+class Array
+    def total # Returns the total number of something in an array
+        cost = 0
+        self.each do |value|
+            cost += value
+        end
+        cost
+    end
+end
+
 class Element
-    attr_reader :price
-    attr_reader :ammount
+    attr_reader :price # The price of the element
+    attr_reader :ammount # What percentage of the element is in your body.
 
     attr_accessor :quantity
 
-	def initialize(price, ammount)
+	def initialize(price, ammount) # What hapens when the class is created
 		@price = price
 		@ammount = ammount
-	end
+    end
+    
+    def quantity_init(grams) # Initalizes how much is in your body
+        @quantity = grams * @ammount
+    end
+
 =begin
 	def price
 		@price
@@ -26,3 +41,4 @@ o.quantity = 15
 puts o.quantity
 puts o.price
 puts o.ammount
+puts [25, 25, 50].total
