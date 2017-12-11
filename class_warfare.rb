c5741a3ef9012a1b5bfe18ad89fda91fd9268b39
gbourdon <34389449+JamesRevere@users.noreply.github.com>
2017-12-10 20:28:23 -0700

    Testing 123?
    Added the elements as objects and did a quick test to see if the prices were the same.

--- class_warfare.rb
+++ class_warfare.rb
@@ -32,8 +32,34 @@ class Element
 =end
 end
 
+price = {o: 0.30, c: 2.40, h: 12.00, n: 0.40, ca: 11.00, p: 4.00, k: 85.00, s: 0.25, na: 7.00, cl: 0.15}
 
 o = Element.new(0.30, 0.65)
+c = Element.new(2.40, 0.18)
+h = Element.new(12, 0.10)
+n = Element.new(0.40, 0.03)
+ca = Element.new(11, 0.015)
+p = Element.new(4, 0.01)
+k = Element.new(85, 0.0035)
+s = Element.new(0.25, 0.0025)
+cl = Element.new(0.15, 0.0015)
+na = Element.new(7, 0.0015)
+
+test = true
+
+test = false unless o.price == price[:o]
+test = false unless c.price == price[:c]
+test = false unless h.price == price[:h]
+test = false unless n.price == price[:n]
+test = false unless ca.price == price[:ca]
+test = false unless p.price == price[:p]
+test = false unless k.price == price[:k]
+test = false unless s.price == price[:s]
+test = false unless cl.price == price[:cl]
+test = false unless na.price == price[:na]
+
+#p test
+
 p o
 
 puts o.price
