2761d1df1988c399bf5ee0dd50aa87134e80c9c7
gbourdon <34389449+JamesRevere@users.noreply.github.com>
2017-12-12 16:00:33 -0700

    Tinkertown
    Added the ability to give the Command Line input directly.

--- how_much_are_you_worth.rb
+++ how_much_are_you_worth.rb
@@ -1,8 +1,11 @@
 # How Much Are You Worth?
 # Have you ever wondered how much you would sell for if you were split up into your individual elements? Well, now you can!
 
-print "How much do you weigh? "
-weightLBS = gets.chomp.to_i
+weightLBS = ARGV[0].to_i
+if weightLBS == 0
+	print "How much do you weigh? "
+	weightLBS = gets.chomp.to_i
+end
 
 elements = Hash.new
 price = {o: 0.30, c: 2.40, h: 12.00, n: 0.40, ca: 11.00, p: 4.00, k: 85.00, s: 0.25, na: 7.00, cl: 0.15}
